2457264f9e4676d206b62cc1b01b9870ea2a8b26
Russell Norris <rsl@luckysneaks.com>
2008-10-08 14:35:00 -0400

    added it_should_set_params

--- lib/lucky_sneaks/controller_spec_helpers.rb
+++ lib/lucky_sneaks/controller_spec_helpers.rb
@@ -371,6 +371,12 @@ module LuckySneaks
         it_should_set :session, name, value, &block
       end
       
+      # Wraps <tt>it_should_set :params</tt>. To specify that the collection should be set
+      # to <tt>nil</tt>, specify the value as :nil instead.
+      def it_should_set_params(name, value = nil, &block)
+        it_should_set :params, name, value, &block
+      end
+      
       # Wraps the various <tt>it_should_render_<i>foo</i></tt> methods:
       # <tt>it_should_render_template</tt>, <tt>it_should_render_partial</tt>,
       # <tt>it_should_render_xml</tt>, <tt>it_should_render_json</tt>,
