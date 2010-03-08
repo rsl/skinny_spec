66c23b450e6534ead5a9e018328122263648acac
RSL <rsl@luckysneaks.com>
2010-03-08 10:27:06 -0500

    added dummy_value_for for non-db-backed 'attributes'

--- lib/lucky_sneaks/common_spec_helpers.rb
+++ lib/lucky_sneaks/common_spec_helpers.rb
@@ -3,7 +3,7 @@ module LuckySneaks
   # methods but you're welcome to use them as needed in your own specs.
   module CommonSpecHelpers
     # Stubs out Time.now and returns value to use when comparing it. Example:
-    # 
+    #
     #   time_now = stub_time_now
     #   @foo.some_method_that_resets_updated_at
     #   @foo.updated_at.should == time_now
@@ -12,9 +12,9 @@ module LuckySneaks
         Time.stub!(:now).and_return(now)
       end
     end
-    
+
     # Returns class for the specified name. Example:
-    # 
+    #
     #   class_for("foo") # => Foo
     def class_for(name)
       name.to_s.constantize
@@ -24,18 +24,18 @@ module LuckySneaks
     end
 
     # Returns instance variable for the specified name. Example:
-    # 
+    #
     #   instance_for("foo") # => @foo
     def instance_for(name)
       instance_variable_get("@#{name.to_s.underscore}")
     end
-    
-    # Wraps a matcher that checks if the receiver contains an <tt>A</tt> element (link) 
+
+    # Wraps a matcher that checks if the receiver contains an <tt>A</tt> element (link)
     # whose <tt>href</tt> attribute is set to the specified path.
     def have_link_to(path)
       have_tag("a[href='#{path}']")
     end
-    
+
     # Returns dummy value for specified attribute based on the datatype expected for that
     # attribute.
     def dummy_value_for(instance, attribute)
@@ -51,9 +51,12 @@ module LuckySneaks
         when :date, :datetime, :time, :timestamp
           actual == 1.week.ago ? 2.years.ago : 1.week.ago
         end
+      else
+        # Not a column
+        "string should work at least for the uses in the internals"
       end
     end
-    
+
     # Returns class description text
     def class_description_text
       if self.class.respond_to?(:description_text)
@@ -64,7 +67,7 @@ module LuckySneaks
         self.class.description
       end
     end
-    
+
     # Returns description text
     def self_description_text
       if respond_to?(:description_text)
@@ -75,7 +78,7 @@ module LuckySneaks
         description
       end
     end
-    
+
     def described_type
       self.class.described_type
     end
